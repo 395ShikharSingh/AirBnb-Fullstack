e430739612f370f07f392eb87909ac4a04743a01
395ShikharSingh <shikharpersonal8@gmail.com>
2025-03-29 21:49:15 +0530

    added port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use("/airbnb", rootRouter);
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
